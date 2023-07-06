a44820616969502600d7b9d0ab5d3cabf6730ac6
DESKTOP-GTC07B0\Sithum <sithumdilshan985@gmail.com>
2023-07-06 16:35:10 +0530

    added

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   // const linkElement = screen.getByText(); 
-  // expect(linkElement).toBeInTheDocument();
+  // expect(linkElement).toBeInTheDocument(); 
   screen.debug();
 });
